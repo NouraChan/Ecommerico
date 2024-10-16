a8ea3232f8cdc925b3d5def9534821d818df10da
norhanali <162762157+NouraChan@users.noreply.github.com>
2024-10-16 12:23:52 +0300

    main app

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,20 +4,20 @@ import footerlogo from '../../img/image 30.png';
 
 export default function Footer() {
 
-let [] = useState();
-    
-        return <>   
-          <section>
+  let [] = useState();
+
+  return <>
+    <section>
       <div className="container-fluid">
         <div className="row footer layout7">
           <div className="col-lg-4">
-          <h5 className="ulhead fw-bold lh-lg"><img src={footerlogo} width={150} alt="" /></h5>
-          
-          <ul className="contacts">
+            <h5 className="ulhead fw-bold lh-lg"><img src={footerlogo} width={150} alt="" /></h5>
+
+            <ul className="contacts">
               <li>
-              <h6 className="fw-semibold lh-lg pe-5">Address</h6></li>
+                <h6 className="fw-semibold lh-lg pe-5">Address</h6></li>
               <li>
-              <h6 className="fw-normal lh-lg">Store & Office Jl. Setrasari Kulon III, No. 10-12, Sukarasa, Sukasari, Bandung, Jawa Barat, Indonesia 40152</h6>
+                <h6 className="fw-normal lh-lg">Store & Office Jl. Setrasari Kulon III, No. 10-12, Sukarasa, Sukasari, Bandung, Jawa Barat, Indonesia 40152</h6>
               </li>
               <li>
                 <h6 className="fw-normal lh-lg">Office Hours</h6>
@@ -29,42 +29,48 @@ let [] = useState();
             </ul>
           </div>
           <div className="col-lg-3 footerlinks fw-normal lh-lg">
-          <h5 className="ulhead fw-bold lh-lg">Get In Touch</h5>
-          <table className="text-white">
-            <tr>
-              <td className=""><h6 className="fw-bold lh-lg pe-5 text-center">Phone</h6></td>
-              <td><h6 className="fw-normal lh-lg">022-20277564</h6></td>
-            </tr>
-            <tr>
-            <td className=""><h6 className="fw-bold lh-lg pe-5 text-center">Service Center</h6></td>
-            <td><h6 className="fw-normal lh-lg">0811-233-8899</h6></td>
-            </tr>
-            <tr>
-            <td className=""><h6 className="fw-bold lh-lg pe-5 text-center">Customer Service</h6></td>
-            <td><h6 className="fw-normal lh-lg">0811-235-9988</h6></td>
-            </tr>
-          </table>
+            <h5 className="ulhead fw-bold lh-lg">Get In Touch</h5>
+            <table className="text-white">
+              <tr>
+                <td className=""><h6 className="fw-bold lh-lg pe-5 text-center">Phone</h6></td>
+                <td><h6 className="fw-normal lh-lg">022-20277564</h6></td>
+              </tr>
+              <tr>
+                <td className=""><h6 className="fw-bold lh-lg pe-5 text-center">Service Center</h6></td>
+                <td><h6 className="fw-normal lh-lg">0811-233-8899</h6></td>
+              </tr>
+              <tr>
+                <td className=""><h6 className="fw-bold lh-lg pe-5 text-center">Customer Service</h6></td>
+                <td><h6 className="fw-normal lh-lg">0811-235-9988</h6></td>
+              </tr>
+            </table>
           </div>
           <div className="col-md-2 footerlinks lh-lg">
             <h5 className="ulhead fw-bold lh-lg">Useful Links</h5>
             <ul>
+              <li><a href="#">Warrany & Complaints</a></li>
+              <li><a href="#">Order & Shipping</a></li>
+              <li><a href="#">Tracking Order</a></li>
               <li><a href="#">About Us</a></li>
-              <li><a href="#">Carrier</a></li>
-              <li><a href="#">We are hiring</a></li>
-              <li><a href="#">Blog</a></li>
+              <li><a href="#">Repair</a></li>
+              <li><a href="#">Terms</a></li>
+              <li><a href="#">FAQ</a></li>
             </ul>
           </div>
           <div className="col-md-2 footerlinks fw-normal lh-lg">
             <h5 className="ulhead fw-bold lh-lg">Campaign</h5>
             <ul>
-              <li><a href="#">Business Marketing</a></li>
-              <li><a href="#">User Analytic</a></li>
-              <li><a href="#">Live Chat</a></li>
-              <li><a href="#">Unlimited Support</a></li>
+            <li><a href="#">Mangenal Arti Cukup</a></li>
+              <li><a href="#">Tell Your Difference</a></li>
+              <li><a href="#">Waykambas</a></li>
+              <li><a href="#">Rebrand</a></li>
+              <li><a href="#">Gallery</a></li>
+              <li><a href="#">Singo</a></li>
+              <li><a href="#">Rakai</a></li>
             </ul>
           </div>
         </div>
       </div>
     </section>
-        </>;
-    };
+  </>;
+};
